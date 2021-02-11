4e56885d6375c43198464a7ccdeb6b6212333f3e
WCIofQMandRA <zichen350@gmail.com>
2021-02-12 00:53:10 +0800

    Update the macro programming part in global_variable.hpp 添加了分析specifier的宏： ____SGV_IS_SPECIFIER(x): 判断x是否是合法的限定符 ____SGV_SPECIFIERS(x): 分析x中含有哪些限定符 ____SGV_SPECIFIERS_VALID: 分析合法的限定符x中含有哪些限定符 global_variable宏现在有4次重载： global_variable(type,name) global_variable(specifier,type,name) global_variable(type,name,init) global_variable(specifier,type,name,init)

--- global_variable.hpp
+++ global_variable.hpp
@@ -104,6 +104,10 @@ public:
 //延迟拼接
 #define ____SGV_CONCAT(x,y) ____SGV_CONCAT_IMPL(x,y)
 #define ____SGV_CONCAT_IMPL(x,y) x##y
+#define ____SGV_CONCAT3(x,y,z) ____SGV_CONCAT3_IMPL(x,y,z)
+#define ____SGV_CONCAT3_IMPL(x,y,z) x##y##z
+#define ____SGV_CONCAT5(x,y,z,w,v) ____SGV_CONCAT5_IMPL(x,y,z,w,v)
+#define ____SGV_CONCAT5_IMPL(x,y,z,w,v) x##y##z##w##v
 
 //条件
 #define ____SGV_IF(STA,THEN,ELSE) ____SGV_CONCAT(____SGV_IF_,STA) (THEN,ELSE)
@@ -119,64 +123,299 @@ public:
 #define ____SGV_IS_PARENS(x) ____SGV_IS_PARENS_IMPL(____SGV_COMMA_V x)
 #define ____SGV_IS_PARENS_IMPL(...) ____SGV_GET_2(__VA_ARGS__,1,0,0)
 
+//判断是否为空，假定x不是元组
+#define ____SGV_EMPTY_1(x) ____SGV_EMPTY_1_IMPL(____SGV_COMMA_V x(),1,0)
+#define ____SGV_EMPTY_1_IMPL(...) ____SGV_GET_2(__VA_ARGS__)
+
 //移除括号
-#define ____SGV_ECHO(x) x
 #define ____SGV_REMOVE_PARENS(x) ____SGV_REMOVE_PARENS_IMPL x
 #define ____SGV_REMOVE_PARENS_IMPL(...) __VA_ARGS__
+//在有括号时移除括号
 #define ____SGV_TRY_REMOVE_PARENS(x)\
 	____SGV_IF(____SGV_IS_PARENS(x),____SGV_REMOVE_PARENS,____SGV_ECHO)(x)
+#define ____SGV_ECHO(x) x//回显参数
+
+//检查x是否是合法的限定符
+#define ____SGV_IS_SPECIFIER(x)\
+	____SGV_IF(____SGV_IS_PARENS(x),____SGV_IS_SPECIFIER_TUPLE,____SGV_IS_SPECIFIER_NTUPLE)(x)
+#define ____SGV_IS_SPECIFIER_TUPLE(x) 0
+//检查非元组x是否是合法的限定符
+#define ____SGV_IS_SPECIFIER_NTUPLE(x) ____SGV_EMPTY_1(____SGV_IS_SPECIFIER_IMPL0(x))
+//检查限定符是否合法，合法时展开为空，否则为一个非空、非元组的宏参数
+#define ____SGV_IS_SPECIFIER_IMPL0(x) ____SGV_IS_SPECIFIER_IMPL1(____SGV_REMOVE_RIGHT_PAR(____SGV_CHECKv(x)))
+//移除未配对的右括号，其中x在展开前括号配对，展开后有游离的右括号
+#define ____SGV_REMOVE_RIGHT_PAR(x) ____SGV_ECHO(x
+//当且仅当x为____SGV_HASv_时，展开为空
+#define ____SGV_IS_SPECIFIER_IMPL1(x) ____SGV_CONCAT(____SGV_IS_SPECIFIER_IMPL2,x)
+#define ____SGV_IS_SPECIFIER_IMPL2____SGV_HASv_
+#define ____SGV_CHECKv(...) ____SGV_HASv_##__VA_ARGS__)
+#define ____SGV_CHECKv1(...) ____SGV_HASv_##__VA_ARGS__)
+#define ____SGV_CHECKv2(...) ____SGV_HASv_##__VA_ARGS__)
+#define ____SGV_CHECKv3(...) ____SGV_HASv_##__VA_ARGS__)
+#define ____SGV_CHECKv4(...) ____SGV_HASv_##__VA_ARGS__)
+#define ____SGV_CHECKv5(...) ____SGV_HASv_##__VA_ARGS__)
+#define ____SGV_HASv_static ____SGV_CHECKv1(
+#define ____SGV_HASv_const ____SGV_CHECKv2(
+#define ____SGV_HASv_thread_local ____SGV_CHECKv3(
+#define ____SGV_HASv_volatile ____SGV_CHECKv4(
+#define ____SGV_HASv_extern ____SGV_CHECKv5(
 
 //检测限定符中是否有extern
-#define ____SGV_CHECK(...) ____SGV_HAS_##__VA_ARGS__)
-#define ____SGV_CHECK1(...) ____SGV_HAS_##__VA_ARGS__)
-#define ____SGV_CHECK2(...) ____SGV_HAS_##__VA_ARGS__)
-#define ____SGV_CHECK3(...) ____SGV_HAS_##__VA_ARGS__)
-#define ____SGV_CHECK4(...) ____SGV_HAS_##__VA_ARGS__)
-#define ____SGV_HAS_static ____SGV_CHECK1(
-#define ____SGV_HAS_const ____SGV_CHECK2(
-#define ____SGV_HAS_thread_local ____SGV_CHECK3(
-#define ____SGV_HAS_volatile ____SGV_CHECK4(
-#define ____SGV_HAS_extern ____SGV_HAS_extern_IMPL(
-#define ____SGV_HAS_ ____SGV_HAS_IMPL(
-#define ____SGV_HAS_IMPL(...) 0
-#define ____SGV_HAS_extern_IMPL(...) 1
+#define ____SGV_CHECK0(...) ____SGV_HAS0_##__VA_ARGS__)
+#define ____SGV_CHECK01(...) ____SGV_HAS0_##__VA_ARGS__)
+#define ____SGV_CHECK02(...) ____SGV_HAS0_##__VA_ARGS__)
+#define ____SGV_CHECK03(...) ____SGV_HAS0_##__VA_ARGS__)
+#define ____SGV_CHECK04(...) ____SGV_HAS0_##__VA_ARGS__)
+#define ____SGV_HAS0_static ____SGV_CHECK01(
+#define ____SGV_HAS0_const ____SGV_CHECK02(
+#define ____SGV_HAS0_thread_local ____SGV_CHECK03(
+#define ____SGV_HAS0_volatile ____SGV_CHECK04(
+#define ____SGV_HAS0_extern ____SGV_HAS0_extern_IMPL(
+#define ____SGV_HAS0_ ____SGV_HAS0_IMPL(
+#define ____SGV_HAS0_IMPL(...) 0
+#define ____SGV_HAS0_extern_IMPL(...) 1
 
-#define ____SGV_EGV_ARG3(specifier,type,name)\
-	____SGV_TRY_REMOVE_PARENS(type) _____SGV_##name##_helper_function();\
-	extern const char *_____SGV_##name##_name;\
-	specifier global_variable_t<____SGV_TRY_REMOVE_PARENS(type),\
-	_____SGV_##name##_helper_function,&_____SGV_##name##_name> name
+//检测限定符中是否有static
+#define ____SGV_CHECK1(...) ____SGV_HAS1_##__VA_ARGS__)
+#define ____SGV_CHECK11(...) ____SGV_HAS1_##__VA_ARGS__)
+#define ____SGV_CHECK12(...) ____SGV_HAS1_##__VA_ARGS__)
+#define ____SGV_CHECK13(...) ____SGV_HAS1_##__VA_ARGS__)
+#define ____SGV_CHECK14(...) ____SGV_HAS1_##__VA_ARGS__)
+#define ____SGV_HAS1_extern ____SGV_CHECK11(
+#define ____SGV_HAS1_const ____SGV_CHECK12(
+#define ____SGV_HAS1_thread_local ____SGV_CHECK13(
+#define ____SGV_HAS1_volatile ____SGV_CHECK14(
+#define ____SGV_HAS1_static ____SGV_HAS1_static_IMPL(
+#define ____SGV_HAS1_ ____SGV_HAS1_IMPL(
+#define ____SGV_HAS1_IMPL(...) 0
+#define ____SGV_HAS1_static_IMPL(...) 1
 
-#define ____SGV_EGV_ARG4(specifier,type,name,init)\
-	____SGV_TRY_REMOVE_PARENS(type) _____SGV_##name##_helper_function()\
+//检测限定符中是否有const
+#define ____SGV_CHECK2(...) ____SGV_HAS2_##__VA_ARGS__)
+#define ____SGV_CHECK21(...) ____SGV_HAS2_##__VA_ARGS__)
+#define ____SGV_CHECK22(...) ____SGV_HAS2_##__VA_ARGS__)
+#define ____SGV_CHECK23(...) ____SGV_HAS2_##__VA_ARGS__)
+#define ____SGV_CHECK24(...) ____SGV_HAS2_##__VA_ARGS__)
+#define ____SGV_HAS2_extern ____SGV_CHECK21(
+#define ____SGV_HAS2_static ____SGV_CHECK22(
+#define ____SGV_HAS2_thread_local ____SGV_CHECK23(
+#define ____SGV_HAS2_volatile ____SGV_CHECK24(
+#define ____SGV_HAS2_const ____SGV_HAS2_const_IMPL(
+#define ____SGV_HAS2_ ____SGV_HAS2_IMPL(
+#define ____SGV_HAS2_IMPL(...) 0
+#define ____SGV_HAS2_const_IMPL(...) 1
+
+//检测限定符中是否有volatile
+#define ____SGV_CHECK3(...) ____SGV_HAS3_##__VA_ARGS__)
+#define ____SGV_CHECK31(...) ____SGV_HAS3_##__VA_ARGS__)
+#define ____SGV_CHECK32(...) ____SGV_HAS3_##__VA_ARGS__)
+#define ____SGV_CHECK33(...) ____SGV_HAS3_##__VA_ARGS__)
+#define ____SGV_CHECK34(...) ____SGV_HAS3_##__VA_ARGS__)
+#define ____SGV_HAS3_extern ____SGV_CHECK31(
+#define ____SGV_HAS3_static ____SGV_CHECK32(
+#define ____SGV_HAS3_thread_local ____SGV_CHECK33(
+#define ____SGV_HAS3_const ____SGV_CHECK34(
+#define ____SGV_HAS3_volatile ____SGV_HAS3_volatile_IMPL(
+#define ____SGV_HAS3_ ____SGV_HAS3_IMPL(
+#define ____SGV_HAS3_IMPL(...) 0
+#define ____SGV_HAS3_volatile_IMPL(...) 1
+
+//检测限定符中是否有volatile
+#define ____SGV_CHECK4(...) ____SGV_HAS4_##__VA_ARGS__)
+#define ____SGV_CHECK41(...) ____SGV_HAS4_##__VA_ARGS__)
+#define ____SGV_CHECK42(...) ____SGV_HAS4_##__VA_ARGS__)
+#define ____SGV_CHECK43(...) ____SGV_HAS4_##__VA_ARGS__)
+#define ____SGV_CHECK44(...) ____SGV_HAS4_##__VA_ARGS__)
+#define ____SGV_HAS4_extern ____SGV_CHECK41(
+#define ____SGV_HAS4_static ____SGV_CHECK42(
+#define ____SGV_HAS4_volatile ____SGV_CHECK43(
+#define ____SGV_HAS4_const ____SGV_CHECK44(
+#define ____SGV_HAS4_thread_local ____SGV_HAS4_thread_local_IMPL(
+#define ____SGV_HAS4_ ____SGV_HAS4_IMPL(
+#define ____SGV_HAS4_IMPL(...) 0
+#define ____SGV_HAS4_thread_local_IMPL(...) 1
+
+//匹配限定符：extern static const volatile thread_local
+//展开结果：
+//对于合法的限定符，为由0/1组成的5位数，第i位表示是否有上一行的第i个限定符
+//对于非法的限定符，为“invalid”
+#define ____SGV_SPECIFIERS(spec)\
+	____SGV_IF(____SGV_IS_SPECIFIER(spec),\
+		____SGV_SPECIFIERS_VALID,____SGV_SPECIFIERS_INVALID)(spec)
+#define ____SGV_SPECIFIERS_INVALID(spec) invalid
+#define ____SGV_SPECIFIERS_VALID(spec)\
+	____SGV_CONCAT5(____SGV_CHECK0(spec),____SGV_CHECK1(spec),\
+	____SGV_CHECK2(spec),____SGV_CHECK3(spec),____SGV_CHECK4(spec))
+
+#if 0
+//
+#define ____SGV_GV00000_ARG2(type,name)
+#define ____SGV_GV00000_ARG3(type,name,init)
+
+//extern
+#define ____SGV_GV10000_ARG2(type,name)
+#define ____SGV_GV10000_ARG3(type,name,init)
+
+//static
+#define ____SGV_GV01000_ARG2(type,name)
+#define ____SGV_GV01000_ARG3(type,name,init)
+
+//extern static
+#define ____SGV_GV11000_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11000_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+
+//const
+#define ____SGV_GV00100_ARG2(type,name)
+#define ____SGV_GV00100_ARG3(type,name,init)
+
+//extern const
+#define ____SGV_GV10100_ARG2(type,name)
+#define ____SGV_GV10100_ARG3(type,name,init)
+
+//static const
+#define ____SGV_GV01100_ARG2(type,name)
+#define ____SGV_GV01100_ARG3(type,name,init)
+
+//extern static const
+#define ____SGV_GV11100_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11100_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+
+//volatile
+#define ____SGV_GV00010_ARG2(type,name)
+#define ____SGV_GV00010_ARG3(type,name,init)
+
+//extern volatile
+#define ____SGV_GV10010_ARG2(type,name)
+#define ____SGV_GV10010_ARG3(type,name,init)
+
+//static volatile
+#define ____SGV_GV01010_ARG2(type,name)
+#define ____SGV_GV01010_ARG3(type,name,init)
+
+//extern static volatile
+#define ____SGV_GV11010_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11010_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+
+//const volatile
+#define ____SGV_GV00110_ARG2(type,name)
+#define ____SGV_GV00110_ARG3(type,name,init)
+
+//extern const volatile
+#define ____SGV_GV10110_ARG2(type,name)
+#define ____SGV_GV10110_ARG3(type,name,init)
+
+//static const volatile
+#define ____SGV_GV01110_ARG2(type,name)
+#define ____SGV_GV01110_ARG3(type,name,init)
+
+//extern static const volatile
+#define ____SGV_GV11110_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11110_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+
+//thread_local
+#define ____SGV_GV00001_ARG2(type,name)
+#define ____SGV_GV00001_ARG3(type,name,init)
+
+//extern thread_local
+#define ____SGV_GV10001_ARG2(type,name)
+#define ____SGV_GV10001_ARG3(type,name,init)
+
+//static thread_local
+#define ____SGV_GV01001_ARG2(type,name)
+#define ____SGV_GV01001_ARG3(type,name,init)
+
+//extern static thread_local
+#define ____SGV_GV11001_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11001_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+
+//const thread_local
+#define ____SGV_GV00101_ARG2(type,name)
+#define ____SGV_GV00101_ARG3(type,name,init)
+
+//extern const thread_local
+#define ____SGV_GV10101_ARG2(type,name)
+#define ____SGV_GV10101_ARG3(type,name,init)
+
+//static const thread_local
+#define ____SGV_GV01101_ARG2(type,name)
+#define ____SGV_GV01101_ARG3(type,name,init)
+
+//extern static const thread_local
+#define ____SGV_GV11101_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11101_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+
+//volatile thread_local
+#define ____SGV_GV00011_ARG2(type,name)
+#define ____SGV_GV00011_ARG3(type,name,init)
+
+//extern volatile thread_local
+#define ____SGV_GV10011_ARG2(type,name)
+#define ____SGV_GV10011_ARG3(type,name,init)
+
+//static volatile thread_local
+#define ____SGV_GV01011_ARG2(type,name)
+#define ____SGV_GV01011_ARG3(type,name,init)
+
+//extern static volatile thread_local
+#define ____SGV_GV11011_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11011_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+
+//const volatile thread_local
+#define ____SGV_GV00111_ARG2(type,name)
+#define ____SGV_GV00111_ARG3(type,name,init)
+
+//extern const volatile thread_local
+#define ____SGV_GV10111_ARG2(type,name)
+#define ____SGV_GV10111_ARG3(type,name,init)
+
+//static const volatile thread_local
+#define ____SGV_GV01111_ARG2(type,name)
+#define ____SGV_GV01111_ARG3(type,name,init)
+
+//extern static const volatile thread_local
+#define ____SGV_GV11111_ARG2(type,name)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#define ____SGV_GV11111_ARG3(type,name,init)\
+	static_assert(false,"conflicting specifiers in declaration of `" #name "' (extern static)")
+#endif
+//#define ____SGV_GV_ARG4(specifier,type,name,init)\
+	____SGV_TRY_REMOVE_PARENS(type) ____SGV_##name##_helper_function()\
 		{return ____SGV_TRY_REMOVE_PARENS(init);}\
-	const char *_____SGV_##name##_name=#name;\
+	const char *____SGV_##name##_name=#name;\
 	specifier global_variable_t<____SGV_TRY_REMOVE_PARENS(type),\
-		_____SGV_##name##_helper_function,&_____SGV_##name##_name> name
+		____SGV_##name##_helper_function,&____SGV_##name##_name> name
 
-#define ____SGV_GV_ARG3(specifier,type,name)\
-	____SGV_TRY_REMOVE_PARENS(type) _____SGV_##name##_helper_function()\
-		{return {};}\
-	const char *_____SGV_##name##_name=#name;\
-	specifier global_variable_t<____SGV_TRY_REMOVE_PARENS(type),\
-		_____SGV_##name##_helper_function,&_____SGV_##name##_name> name
-
+//4重载
+#define ____SGV_global_variable(...) ____SGV_CONCAT(____SGV_GV_ARG,____SGV_GET_7(__VA_ARGS__,7,6,5,4,3,2,1))(__VA_ARGS__)
+//global_variable(type,name)
+#define ____SGV_GV_ARG2(type,name)\
+	____SGV_CONCAT3(____SGV_GV,00000,_ARG2)(type,name)
+//
+#define ____SGV_GV_ARG3(x,y,z)\
+	____SGV_IF(____SGV_IS_SPECIFIER(x),____SGV_GV_ARG30,____SGV_GV_ARG31)(x,y,z)
+//global_variable(specifier,type,name)
+#define ____SGV_GV_ARG30(specifier,type,name)\
+	____SGV_CONCAT3(____SGV_GV,____SGV_SPECIFIERS_VALID(specifier),_ARG2)(type,name)
+//global_variable(type,name,init)
+#define ____SGV_GV_ARG31(type,name,init)\
+	____SGV_CONCAT3(____SGV_GV,00000,_ARG3)(type,name,init)
+//global_variable(specifier,type,name,init)
 #define ____SGV_GV_ARG4(specifier,type,name,init)\
-	____SGV_TRY_REMOVE_PARENS(type) _____SGV_##name##_helper_function()\
-		{return ____SGV_TRY_REMOVE_PARENS(init);}\
-	const char *_____SGV_##name##_name=#name;\
-	specifier global_variable_t<____SGV_TRY_REMOVE_PARENS(type),\
-		_____SGV_##name##_helper_function,&_____SGV_##name##_name> name
-
-#define ____SGV_global_variable_ARG3(specifier,type,name)\
-	____SGV_IF(____SGV_CHECK(specifier),____SGV_EGV_ARG3,____SGV_GV_ARG3)\
-		(specifier,type,name)
-
-#define ____SGV_global_variable_ARG4(specifier,type,name,init)\
-	____SGV_IF(____SGV_CHECK(specifier),____SGV_EGV_ARG4,____SGV_GV_ARG4)\
-		(specifier,type,name,init)
-
-#define ____SGV_global_variable(...) ____SGV_CONCAT(____SGV_global_variable_ARG,____SGV_GET_7(__VA_ARGS__,7,6,5,4,3,2,1))(__VA_ARGS__)
+	____SGV_CONCAT3(____SGV_GV,____SGV_SPECIFIERS(specifier),_ARG3)(type,name,init)
 
 //源代码兼容v1.0.0
 #define ____SGV_extern_global_variable ____SGV_global_variable
